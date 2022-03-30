bb46c217305e048ec243d97c30616e74d5f26911
nijennono <98542617+nijennono@users.noreply.github.com>
2022-03-30 14:55:02 +0200

    Update Weizenmehl.js

--- Weizenmehl.js
+++ Weizenmehl.js
@@ -1,266 +1,226 @@
-let country = "de"; // replace with 'at' for shops in Austria
-let storeId = 433;
-let param = args.widgetParameter;
+let storeId = 433
+let param = args.widgetParameter
 if (param != null && param.length > 0) {
-  if (param.indexOf(";") > 0) {
-    const paramSplit = param.split(";");
-    storeId = paramSplit[0];
-    country = paramSplit[1].toLowerCase();
-  } else {
-    storeId = param;
-  }
+    storeId = param
 }
 
-const widget = new ListWidget();
-const storeInfo = await fetchStoreInformation();
-const storeCapacity = await fetchAmountOfPaper();
-const isOnlineAvailable = await isAvailableOnline();
-await createWidget();
+const widget = new ListWidget()
+const storeInfo = await fetchStoreInformation()
+const storeCapacityW550 = await fetchAmountOfW550()
+const storeCapacityW1050 = await fetchAmountOfW1050()
+const storeCapacityWeizenVK = await fetchAmountOfWeizenVK()
+await createWidget()
 
 // used for debugging if script runs inside the app
 if (!config.runsInWidget) {
-  await widget.presentSmall();
+    await widget.presentSmall()
 }
-Script.setWidget(widget);
-Script.complete();
+Script.setWidget(widget)
+Script.complete()
 
 // build the content of the widget
 async function createWidget() {
-  const logoImg = await getImage("dm-logo.png");
 
-  widget.setPadding(6, 10, 8, 10);
-  const titleFontSize = 12;
-  const detailFontSize = 36;
+    const logoImg = await getImage('dm-logo.png')
 
-  const logoStack = widget.addStack();
-  logoStack.addSpacer();
-  const logoImageStack = logoStack.addStack();
-  logoStack.layoutHorizontally();
-  logoImageStack.backgroundColor = new Color("#ffffff", 1.0);
-  logoImageStack.cornerRadius = 8;
-  const wimg = logoImageStack.addImage(logoImg);
-  wimg.imageSize = new Size(32, 32);
-  wimg.rightAlignImage();
+    widget.setPadding(8,8,8,8)
+    const titleFontSize = 12
+    const detailFontSize = 36
 
-  const paperText = widget.addText("WEIZENMEHL");
-  paperText.font = Font.semiboldRoundedSystemFont(11);
+    let titelzeile = widget.addStack()
+    titelzeile.layoutVertically()
+    const titel = titelzeile.addText("meHl bei dm")
+    titel.font = Font.boldSystemFont(14)
+    widget.addSpacer(1)
 
-  widget.addSpacer(8);
+    // weizen 550
+    let row = widget.addStack()
+    row.layoutHorizontally()
 
-  const icon = await getImage("covidtest.png");
-  let row = widget.addStack();
-  row.layoutHorizontally();
-  row.addSpacer(2);
-  const iconImg = row.addImage(icon);
-  iconImg.imageSize = new Size(32, 32);
-  row.addSpacer(12);
+    let column = row.addStack()
+    column.layoutVertically()
 
-  let column = row.addStack();
-  column.layoutVertically();
+    const W550Text = column.addText("Weizenmehl 550: ")
+    W550Text.font = Font.mediumRoundedSystemFont(11)
 
-  const storeText = column.addText("STORE");
-  storeText.textOpacity = 0.8;
-  storeText.font = Font.mediumRoundedSystemFont(10);
-  const packageCount = column.addText(storeCapacity.toString());
-  if (storeCapacity > 999) {
-    packageCount.font = Font.mediumRoundedSystemFont(16);
-  } else {
-    packageCount.font = Font.mediumRoundedSystemFont(19);
-  }
-  //   packageCount.minimumScaleFactor = 0.8
-  if (storeCapacity < 5) {
-    packageCount.textColor = new Color("#E50000");
-  } else {
-    packageCount.textColor = new Color("#00CD66");
-  }
+    const W550Count = row.addText(storeCapacityW550.toString())
+    W550Count.font = Font.mediumRoundedSystemFont(11)
+    if (storeCapacityW550 < 6) {
+        W550Count.textColor = new Color("#E50000")
+    } else {
+        W550Count.textColor = new Color("#00CD66")
+    }
+    // widget.addSpacer(4)
 
-  row.addSpacer(12);
+    // weizen 1050
+    let row2 = widget.addStack()
+    row2.layoutHorizontally()
 
-  let column2 = row.addStack();
-  column2.layoutVertically();
-  const onlineText = column2.addText("ONLINE");
-  onlineText.textOpacity = 0.8;
-  onlineText.font = Font.mediumRoundedSystemFont(10);
-  column2.addSpacer(3);
+    let column2 = row2.addStack()
+    column2.layoutVertically()
 
-  let onlineAvailableIcon;
-  if (isOnlineAvailable) {
-    onlineAvailableIcon = "✅";
-  } else {
-    onlineAvailableIcon = "⛔️";
-  }
-  const onlineAvailableText = column2.addText(onlineAvailableIcon);
+    const W1050Text = column2.addText("Weizenmehl 1050: ")
+    W1050Text.font = Font.mediumRoundedSystemFont(11)
 
-  if (storeCapacity > 999) {
-    onlineAvailableText.font = Font.regularSystemFont(11);
-  } else {
-    onlineAvailableText.font = Font.regularSystemFont(13);
-  }
+    const W1050Count = row2.addText(storeCapacityW1050.toString())
+    W1050Count.font = Font.mediumRoundedSystemFont(11)
+    if (storeCapacityW1050 < 6) {
+        W1050Count.textColor = new Color("#E50000")
+    } else {
+        W1050Count.textColor = new Color("#00CD66")
+    }
+    // widget.addSpacer(4)
 
-  widget.addSpacer(10);
+    // weizen vk
+    let row3 = widget.addStack()
+    row3.layoutHorizontally()
 
-  const row2 = widget.addStack();
-  row2.layoutVertically();
+    let column3 = row3.addStack()
+    column3.layoutVertically()
 
-  const street = row2.addText(storeInfo.address.street);
-  street.font = Font.regularSystemFont(11);
-  street.minimumScaleFactor = 0.8;
-  street.lineLimit = 2;
+    const WeizenVKText = column3.addText("Weizen-Vollkorn: ")
+    WeizenVKText.font = Font.mediumRoundedSystemFont(11)
 
-  const zipCity = row2.addText(
-    storeInfo.address.zip + " " + storeInfo.address.city
-  );
-  zipCity.font = Font.regularSystemFont(11);
-  zipCity.minimumScaleFactor = 0.8;
-  zipCity.lineLimit = 1;
+    const WeizenVKCount = row3.addText(storeCapacityWeizenVK.toString())
+    WeizenVKCount.font = Font.mediumRoundedSystemFont(11)
+    if (storeCapacityWeizenVK < 6) {
+        WeizenVKCount.textColor = new Color("#E50000")
+    } else {
+        WeizenVKCount.textColor = new Color("#00CD66")
+    }
+    // widget.addSpacer(4)
+	
+    // shop info & logo
+    let infologo = widget.addStack()
+    infologo.layoutHorizontally()
 
-  let currentTime = new Date().toLocaleTimeString("de-DE", {
-    hour: "numeric",
-    minute: "numeric",
-  });
-  let currentDay = new Date().getDay();
-  let isOpen;
-  if (currentDay > 0) {
-    const todaysOpeningHour =
-      storeInfo.openingHours[currentDay - 1].timeRanges[0].opening;
-    const todaysClosingHour =
-      storeInfo.openingHours[currentDay - 1].timeRanges[0].closing;
-    const range = [todaysOpeningHour, todaysClosingHour];
-    isOpen = isInRange(currentTime, range);
-  } else {
-    isOpen = false;
-  }
+    let infos = infologo.addStack()
+    infos.layoutVertically()
 
-  let shopStateText;
-  if (isOpen) {
-    shopStateText = row2.addText("GEÖFFNET");
-    shopStateText.textColor = new Color("#00CD66");
-  } else {
-    shopStateText = row2.addText("GESCHLOSSEN");
-    shopStateText.textColor = new Color("#E50000");
-  }
-  shopStateText.font = Font.semiboldSystemFont(10);
+    const street = infos.addText(storeInfo.address.street)
+    street.font = Font.regularSystemFont(8)
+
+    const zipCity = infos.addText(storeInfo.address.zip + " " + storeInfo.address.city)
+    zipCity.font = Font.regularSystemFont(8)
+
+    let currentTime = new Date().toLocaleTimeString('de-DE', { hour: "numeric", minute: "numeric" })
+    let currentDay = new Date().getDay()
+    let isOpen
+    if (currentDay > 0) {
+        const todaysOpeningHour = storeInfo.openingHours[currentDay-1].timeRanges[0].opening
+        const todaysClosingHour = storeInfo.openingHours[currentDay-1].timeRanges[0].closing
+        const range = [todaysOpeningHour, todaysClosingHour];
+        isOpen = isInRange(currentTime, range)
+    } else {
+        isOpen = false
+    }
+
+    let shopStateText
+    if (isOpen) {
+        shopStateText = infos.addText('Geöffnet')
+        shopStateText.textColor = new Color("#00CD66")
+    } else {
+        shopStateText = infos.addText('Geschlossen')
+        shopStateText.textColor = new Color("#E50000")
+    }
+    shopStateText.font = Font.mediumSystemFont(8)
+    infologo.addSpacer(20)
+
+    const logoImageStack = infologo.addStack()
+    logoImageStack.layoutVertically()
+    logoImageStack.addSpacer(0)
+    const logoImageStack2 = logoImageStack.addStack()
+    logoImageStack2.backgroundColor = new Color("#ffffff", 1.0)
+    logoImageStack2.cornerRadius = 6
+    const logo = logoImageStack2.addImage(logoImg)
+    logo.imageSize = new Size(25, 25)
+    logo.rightAlignImage()
+    
 }
 
-// fetches the amount of milk packages
-async function fetchAmountOfPaper() {
-  let url;
-  let counter = 0;
-  if (country.toLowerCase() === "at") {
-    // Austria
-    const array = ["188896","114408"];
-    for (var i = 0; i < array.length; i++) {
-      let currentItem = array[i];
-      url =
-        "https://products.dm.de/store-availability/AT/products/dans/" +
-        currentItem +
-        "/stocklevel?storeNumbers=" +
-        storeId;
-      let req = new Request(url);
-      let apiResult = await req.loadJSON();
-      if (req.response.statusCode == 200) {
-        counter += apiResult.storeAvailability[0].stockLevel;
-      }
-    }
-  } else {
-    // Germany
-    url = "https://products.dm.de/store-availability/DE/availability?dans=798989,819862,797046,800021&storeNumbers=" + storeId;
-    const req = new Request(url);
-    const apiResult = await req.loadJSON();
-    for (var i in apiResult.storeAvailabilities) {
-      if (apiResult.storeAvailabilities[i][0].stockLevel) {
-        counter += apiResult.storeAvailabilities[i][0].stockLevel;
-      }
-    }
-  }
-  return counter;
+// fetches the amount of weizen 550 packages
+async function fetchAmountOfW550() {
+    let url
+    let counter = 0
+        url = 'https://products.dm.de/store-availability/DE/availability?dans=459912&storeNumbers=' + storeId
+        const req = new Request(url)
+        const apiResult = await req.loadJSON()
+        for (var i in apiResult.storeAvailabilities) {
+            counter += apiResult.storeAvailabilities[i][0].stockLevel
+        }
+    return counter
 }
 
-async function isAvailableOnline() {
-  let url;
-  if (country.toLowerCase() === "at") {
-    url =
-      "https://products.dm.de/product/at/search?productQuery=%3Arelevance%3Adan%3A188896";
-  } else {
-    url =
-      "https://products.dm.de/product/de/search?productQuery=%3Arelevance%3Adan%3A796724&purchasableOnly=false&hideFacets=false&hideSorts=false&pageSize=30";
-  }
-  let req = new Request(url);
-  let apiResult = await req.loadJSON();
-
-  if (req.response.statusCode == 200) {
-    for (var i = 0; i < apiResult.products.length; i++) {
-      if (apiResult.products[i].purchasable) {
-        return true;
-      }
-    }
-  }
-  return false;
+// fetches the amount of weizen 1050 packages
+async function fetchAmountOfW1050() {
+    let url
+    let counter = 0
+        url = 'https://products.dm.de/store-availability/DE/availability?dans=468120&storeNumbers=' + storeId
+        const req = new Request(url)
+        const apiResult = await req.loadJSON()
+        for (var i in apiResult.storeAvailabilities) {
+            counter += apiResult.storeAvailabilities[i][0].stockLevel
+        }
+    return counter
 }
+
+// fetches the amount of weizen vollkorn packages
+async function fetchAmountOfWeizenVK() {
+    let url
+    let counter = 0
+        url = 'https://products.dm.de/store-availability/DE/availability?dans=468178&storeNumbers=' + storeId
+        const req = new Request(url)
+        const apiResult = await req.loadJSON()
+        for (var i in apiResult.storeAvailabilities) {
+            counter += apiResult.storeAvailabilities[i][0].stockLevel
+        }
+    return counter
+}
+
 
 // fetches information of the configured store, e.g. opening hours, address etc.
 async function fetchStoreInformation() {
-  let url;
-  if (country.toLowerCase() === "at") {
-    url =
-      "https://store-data-service.services.dmtech.com/stores/item/at/" +
-      storeId;
-    widget.url =
-      "https://www.dm.at/keine-marke-coronavirus-antigen-schnelltest-fuer-zuhause-p2099999042083.html";
-  } else {
-    url =
-      "https://store-data-service.services.dmtech.com/stores/item/de/" +
-      storeId;
-    widget.url =
-      "https://www.dm.de/search?query=weizenmehl&searchType=product";
-  }
-  let req = new Request(url);
-  let apiResult = await req.loadJSON();
-  return apiResult;
+    let url
+        url = 'https://store-data-service.services.dmtech.com/stores/item/de/' + storeId
+        widget.url = 'https://www.dm.de/search?query=mehl&searchType=product'
+    let req = new Request(url)
+    let apiResult = await req.loadJSON()
+    return apiResult
 }
 
 // checks whether the store is currently open or closed
 function isInRange(value, range) {
-  return value >= range[0] && value <= range[1];
+    return value >= range[0] && value <= range[1];
 }
 
 // get images from local filestore or download them once
 async function getImage(image) {
-  let fm = FileManager.local();
-  let dir = fm.documentsDirectory();
-  let path = fm.joinPath(dir, image);
-  if (fm.fileExists(path)) {
-    return fm.readImage(path);
-  } else {
-    // download once
-    let imageUrl;
-    switch (image) {
-      case "dm-logo.png":
-        imageUrl =
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Dm_Logo.svg/300px-Dm_Logo.svg.png";
-        break;
-      case "covidtest.png":
-        imageUrl = "https://i.imgur.com/Nra1754.png";
-        break;
-      default:
-        console.log(`Sorry, couldn't find ${image}.`);
+    let fm = FileManager.local()
+    let dir = fm.documentsDirectory()
+    let path = fm.joinPath(dir, image)
+    if (fm.fileExists(path)) {
+        return fm.readImage(path)
+    } else {
+        // download once
+        let imageUrl
+        switch (image) {
+            case 'dm-logo.png':
+                imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Dm_Logo.svg/300px-Dm_Logo.svg.png"
+                break
+            default:
+                console.log(`Sorry, couldn't find ${image}.`);
+        }
+        let iconImage = await loadImage(imageUrl)
+        fm.writeImage(path, iconImage)
+        return iconImage
     }
-    let iconImage = await loadImage(imageUrl);
-    fm.writeImage(path, iconImage);
-    return iconImage;
-  }
 }
 
 // helper function to download an image from a given url
 async function loadImage(imgUrl) {
-  const req = new Request(imgUrl);
-  return await req.loadImage();
+    const req = new Request(imgUrl)
+    return await req.loadImage()
 }
 
 
-
-
-
-//
-// make sure to copy everything!
-//
+// End of script
